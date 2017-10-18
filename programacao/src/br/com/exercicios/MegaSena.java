63964caed8c2193d7598d40e08087b0cffab0002
pocki <pocki@DESKTOP-GB500HP>
2017-10-18 18:41:27 -0300

    Inicial Ana

---
+++ programacao/src/br/com/exercicios/MegaSena.java
@@ -0,0 +1,57 @@
+package br.com.exercicios;
+
+import java.util.Scanner;
+
+public class MegaSena {
+
+	public static void main(String[] args) {
+	
+		
+		Scanner scan = new Scanner(System.in);
+		int [] numCartao = new int [60];
+		int [] aposta = new int [6];
+		
+		
+		int cont =0;
+		while(cont < 6) {
+			boolean repetido = false;
+				
+			borges.alan10@gmail.com			
+							
+					System.out.println("Digite sua aposta");
+					aposta[cont] = scan.nextInt();
+					
+					
+					if((aposta[cont] >0) && (aposta[cont] <= 60)) {
+						
+						repetido =true;
+						
+								} else {
+									
+									return;
+								}
+					
+					repetido=false;
+					if(repetido) {
+						
+						
+					}
+					
+					
+					
+					
+					cont++;		
+		}
+	
+		
+			
+			
+
+		
+		
+		
+		
+
+	}
+
+}
